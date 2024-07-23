c7c95982e6813d19f32cc1296fe3cfb49f8fd906
jagaldino <arthurgaldino811@gmail.com>
2024-07-23 10:14:35 -0300

    Inicial commit

---
+++ Program.cs
@@ -0,0 +1,70 @@
+﻿using System;
+using System.Threading;
+
+namespace Stopwatch
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // Start(6);
+            Menu();
+        }
+
+        static void Menu()
+        {
+            Console.Clear();
+            Console.WriteLine("S = Segundo => 10s = 10 segundos");
+            Console.WriteLine("M = Minuto => 1m = 1 minuto");
+            Console.WriteLine("0 = Sair");
+            Console.WriteLine("Quanto tempo deseja contar?");
+
+            string data = Console.ReadLine().ToLower();
+            char type = char.Parse(data.Substring(data.Length - 1, 1));
+            int time = int.Parse(data.Substring(0, data.Length - 1));
+            int multiplica = 1;
+
+            if (time == 0)
+            {
+                System.Environment.Exit(0);
+            }
+
+            if (type == 'm')
+            {
+                multiplica = 60;
+            }
+
+            PreStart(time * multiplica);
+        }
+
+        static void PreStart(int time)
+        {
+            Console.Clear();
+            Console.WriteLine("Read...");
+            Thread.Sleep(1000);
+            Console.WriteLine("Set...");
+            Thread.Sleep(1000);
+            Console.WriteLine("Go...");
+            Thread.Sleep(2500);
+
+            Start(time);
+        }
+        static void Start(int time)
+        {
+            int currentTime = 0;
+
+            while (currentTime != time)
+            {
+                Console.Clear();
+                currentTime++;
+                Console.WriteLine(currentTime);
+                Thread.Sleep(1000);
+            }
+
+            Console.Clear();
+            Console.WriteLine("Stopwatch finalizado");
+            Thread.Sleep(2500);
+            Menu();
+        }
+    }
+}
---
+++ README.md
@@ -0,0 +1,46 @@
+# ⏱️ Stopwatch Console Application
+
+Este é um simples aplicativo de cronômetro console em C# que permite ao usuário contar segundos ou minutos. O usuário pode especificar o tempo desejado em segundos ou minutos e o cronômetro contará até esse tempo, exibindo uma contagem no console.
+
+
+## ⚙️ Funcionalidades
+
+- ⏲️ Contagem de tempo em segundos (`s`) ou minutos (`m`).
+- 📢 Exibição de uma contagem regressiva "Read...", "Set...", "Go..." antes do início do cronômetro.
+- 🔁 Retorno ao menu principal após a conclusão do cronômetro.
+- ❌ Opção de sair do programa inserindo `0`.
+
+## 🚀 Como Usar
+
+1. Clone ou faça o download do repositório.
+2. Compile o código utilizando o compilador C# de sua preferência.
+3. Execute o programa compilado.
+
+### 📋 Menu Principal
+
+No menu principal, você verá as seguintes opções:
+
+- `S = Segundo => 10s = 10 segundos`
+- `M = Minuto => 1m = 1 minuto`
+- `0 = Sair`
+
+Insira o tempo desejado no formato especificado e pressione Enter. O cronômetro iniciará a contagem após uma breve contagem regressiva.
+
+### 💡 Exemplo de Uso
+
+- Para contar 10 segundos, digite `10s` e pressione Enter.
+- Para contar 1 minuto, digite `1m` e pressione Enter.
+- Para sair do programa, digite `0` e pressione Enter.
+
+## 🛠️ Tecnologias Utilizadas
+
+- C#
+- .NET Core
+
+## 📄 Licença
+
+Este projeto está licenciado sob a Licença MIT.
+
+---
+
+Feito com ❤️ por João Arthur (https://github.com/jagaldino)
